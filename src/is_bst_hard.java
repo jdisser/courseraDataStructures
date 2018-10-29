3abd2b64e2994a47389a0e55d274713878ba08ca
jrdisser <jimdisser@altocirrusapps.com>
2018-10-29 13:33:19 -0600

    added tree is valid algorithm components to is_bst_hard

--- src/is_bst_hard.java
+++ src/is_bst_hard.java
@@ -1,4 +1,8 @@
 import java.util.*;
+
+//import is_bst.FastScanner;
+//import is_bst.IsBST.Node;
+
 import java.io.*;
 
 public class is_bst_hard {
@@ -35,21 +39,78 @@ public class is_bst_hard {
 
         int nodes;
         Node[] tree;
+        ArrayList<Integer> leaves;
 
+        
+        private int find(int ri, int key) {
+        	int result = -1;
+        	
+        	if(ri == -1)										//null does not hold key
+        		return result;
+        	
+        	if(tree[ri].key == key)								//found the key
+        		return ri;
+        	
+        	if(tree[ri].left == -1 && tree[ri].right == -1)		//if a leave is reached and the key not found the tree is bad
+        		return result;
+        	
+        	if(key > tree[ri].key) {
+        		if(tree[ri].right != -1) {							//if the subtree is null key not found
+        			if(tree[tree[ri].right].key < tree[ri].key)		//or if the subtree root is invalid key not found
+            			return result;
+            		else
+            			result = find(tree[ri].right, key);			//if subtree valid search for the key
+        		} else
+        			return result;
+        		
+        	}
+        	
+        	if(key < tree[ri].key) {
+        		if(tree[ri].left != -1) {
+        			if(tree[tree[ri].left].key > tree[ri].key)
+            			return result;
+            		else
+            			result = find(tree[ri].left, key);
+        		}
+        		
+        	}
+        	
+        	return result;
+        }
+        
+        
+        
+        
+        //TODO: assume tree[0] is root???
         void read() throws IOException {
             FastScanner in = new FastScanner();
             nodes = in.nextInt();
             tree = new Node[nodes];
+            leaves = new ArrayList<Integer>();
             for (int i = 0; i < nodes; i++) {
                 tree[i] = new Node(in.nextInt(), in.nextInt(), in.nextInt());
+                if(tree[i].left == -1 && tree[i].right == -1)
+                	leaves.add(i);
             }
         }
 
         boolean isBinarySearchTree() {
-          // Implement correct algorithm here
-          return true;
+        	boolean result = true;
+        	
+          if(leaves.isEmpty())
+        	  return result;				//case of empty tree or single node
+          
+          for(int l : leaves) {
+        	  if(find(0,tree[l].key) == -1) {
+        		  result = false;
+        		  break;
+        	  }
+          }
+         
+          return result;
         }
     }
+
 
     static public void main(String[] args) throws IOException {
         new Thread(null, new Runnable() {
@@ -64,7 +125,7 @@ public class is_bst_hard {
     public void run() throws IOException {
         IsBST tree = new IsBST();
         tree.read();
-        if (tree.solve()) {
+        if (tree.isBinarySearchTree()) {
             System.out.println("CORRECT");
         } else {
             System.out.println("INCORRECT");
